7ebdb7ce88e3b8362c6bda0a0a5bed477c635045
Tushar Salunke <Tushar Salunke@DESKTOP-EO1OBGM>
2022-02-19 17:43:33 +0530

    Add project files.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,152 @@
+﻿using ExamProduct.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+
+namespace ExamProduct.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            List<Product> lstProduct = new List<Product>();
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=ExamDatabase;Integrated Security=True";
+            cn.Open();
+
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cn;
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+          
+            cmd.CommandText = "ProcedureForSelect";
+
+            try
+            {
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+
+                    lstProduct.Add(new Product {ProductId=(int)dr["ProductId"],ProductName=dr["ProductName"].ToString(),Rate=(Decimal)dr["Rate"],Description=dr["Description"].ToString(),CategoryName=dr["CategoryName"].ToString() });
+                }
+                dr.Close();
+
+            }
+            catch (Exception e)
+            {
+
+                ViewBag.IndexErr = e.Message;
+
+            }
+
+            finally {
+
+                cn.Close();
+            }
+
+
+
+            return View(lstProduct);
+        }
+
+       
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int id=0)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=ExamDatabase;Integrated Security=True";
+            cn.Open();
+
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cn;
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandText = "Select * from Products where ProductId="+id;
+            Product product = null;
+            try
+            {
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+
+                    product = new Product { ProductId = (int)dr["ProductId"], ProductName = dr["ProductName"].ToString(), Rate = (Decimal)dr["Rate"], Description = dr["Description"].ToString(), CategoryName = dr["CategoryName"].ToString() };
+
+                }
+                dr.Close();
+
+
+            }
+            catch (Exception e)
+            {
+                ViewBag.EditError = e.Message;
+
+            }
+            finally {
+                cn.Close();
+            
+            }
+
+
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Product product, int id=0)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=ExamDatabase;Integrated Security=True";
+            cn.Open();
+
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cn;
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.CommandText = "ProcedureForUpdate";
+           
+            cmd.Parameters.AddWithValue("@ProductId",product.ProductId);
+            cmd.Parameters.AddWithValue("@ProductName", product.ProductName);
+            cmd.Parameters.AddWithValue("@Rate", product.Rate);
+            cmd.Parameters.AddWithValue("@Description", product.Description);
+            cmd.Parameters.AddWithValue("@CategoryName", product.CategoryName);
+            try
+            {
+
+                cmd.ExecuteNonQuery();
+
+                RedirectToAction("Index");
+               
+
+            }
+            catch (Exception e)
+            {
+                ViewBag.EditError = e.Message;
+
+            }
+            finally
+            {
+                cn.Close();
+
+            }
+            return View();
+        }
+
+
+      [ChildActionOnly]
+        public ActionResult MyPartialView()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ExamProduct.Models
+{
+    public class Product
+    {
+        [Required (ErrorMessage ="Enter Product Id")]
+        
+        [Display(Name ="Product Id")]
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "Enter Product Name")]
+        [DataType(DataType.Text,ErrorMessage ="Enter Valid Name")]
+        [StringLength(10,ErrorMessage ="The Value of {0} should be {1} of lenghth")]
+        [MinLength(3,ErrorMessage ="minimum length of {0} should be {1}")]
+        [Display(Name = "Product Name")]
+
+        public String ProductName { get; set; }
+
+        [Range(0,200,ErrorMessage ="value should be beetween 0-200")]
+        [Required(ErrorMessage = "Enter Product Rate")]
+        [Display(Name = "Rate")]
+        public Decimal Rate { get; set; }
+
+        [Required(ErrorMessage = "Enter Description")]
+        [DataType(DataType.Text, ErrorMessage = "Enter Valid Name")]
+        [StringLength(20, ErrorMessage = "The Value of {0} should be {1} of lenghth")]
+        [MinLength(3, ErrorMessage = "minimum length of {0} should be {1}")]
+        [Display(Name = "Description")]
+        public String Description { get; set; }
+
+        [Required(ErrorMessage = "Enter Category")]
+        [DataType(DataType.Text, ErrorMessage = "Enter Valid Name")]
+        [StringLength(20, ErrorMessage = "The Value of {0} should be {1} of lenghth")]
+        [MinLength(3, ErrorMessage = "minimum length of {0} should be {1}")]
+        [Display(Name = "Category")]
+        public String CategoryName { get; set; }
+
+    }
+}
+
+
+/*ProductId int - Primary Key
+ProductName varchar(50)
+Rate Decimal(18,2)
+Description varchar(200)
+CategoryName varchar(50)*/
